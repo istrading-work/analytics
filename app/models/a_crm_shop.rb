443f49cda41b29b2322a8fdff1e43a05738b8f3c
jooshka <jooshka@mail.ru>
2016-12-04 21:00:49 +0300

    Add scope by_user

--- app/models/a_crm_shop.rb
+++ app/models/a_crm_shop.rb
@@ -11,7 +11,15 @@
 
 class ACrmShop < ApplicationRecord
   self.primary_key = 'code'
-
+  
+  has_many :a_user_shop
+  has_many :a_admin_users, through: :a_user_shop  
+  
   scope :active, -> { where( :is_active => true) }
-
+  
+  scope :by_user, -> (p_user_id) {
+    joins(:a_admin_users)
+   .where("a_admin_users.id = #{p_user_id}")
+  }
+  
 end
